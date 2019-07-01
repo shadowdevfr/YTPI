27bd477f4640149ed97b4b9c57037fb3ae61cb9f
PixiWebDev <33694505+pixiwebdevfr@users.noreply.github.com>
2019-07-01 11:51:05 +0200

    Update YTPi.sh

--- YTPi.sh
+++ YTPi.sh
@@ -1,7 +1,2 @@
-echo 'To use YTPi, simply change the settings that you gonna see and save'
-echo 'the file and close. Starting in 3s'
-sleep 3s
-cd YTPi
-sudo wget http://dl.api-lw.fr/dwl.sh
 sudo nano dwl.sh
 sh dwl.sh
